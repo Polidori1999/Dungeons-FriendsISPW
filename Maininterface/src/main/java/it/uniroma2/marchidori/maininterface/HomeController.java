880ce7b9499bea6d7a8ad453bdf588984d705370
polidori1999 <leo.polidori99@gmail.com>
2025-01-27 17:35:28 +0100

    Creata entity lobby e aggiornata home

--- Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/HomeController.java
+++ Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/HomeController.java
@@ -1,5 +1,7 @@
 package it.uniroma2.marchidori.maininterface;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ListCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -15,13 +19,11 @@ import java.io.IOException;
 public class HomeController {
 
     @FXML
-    private ComboBox<?> ComboBox1;
-
+    private ComboBox<String> ComboBox1; // Live/On-line
     @FXML
-    private ComboBox<?> ComboBox2;
-
+    private ComboBox<String> ComboBox2; // Duration
     @FXML
-    private ComboBox<?> ComboBox3;
+    private ComboBox<String> ComboBox3; // Num Players
 
     @FXML
     private Button Consult_rules;
@@ -39,25 +41,121 @@ public class HomeController {
     private Button Mychar;
 
     @FXML
+    private ListView<Lobby> lobbyListView; // Modifica ListView per usare oggetti Lobby
+
+    @FXML
     private Button userButton;
+
+    // Lista simulata di lobby
+    private ObservableList<Lobby> allLobbies = FXCollections.observableArrayList(
+            new Lobby("Lobby1", "Online", "Singola", 4),
+            new Lobby("Lobby2", "Presenza", "Campagna", 6),
+            new Lobby("Lobby3", "Online", "Singola", 3),
+            new Lobby("Lobby4", "Presenza", "Campagna", 8)
+    );
+
+    private ObservableList<Lobby> filteredLobbies = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        // Aggiungi opzioni alle ComboBox
+        ComboBox1.setItems(FXCollections.observableArrayList("Online", "Presenza"));
+        ComboBox2.setItems(FXCollections.observableArrayList("Singola", "Campagna"));
+        ComboBox3.setItems(FXCollections.observableArrayList("2", "3", "4", "5", "6", "7", "8"));
+
+        // Aggiungi listener per il filtro
+        ComboBox1.valueProperty().addListener((obs, oldVal, newVal) -> filterLobbies());
+        ComboBox2.valueProperty().addListener((obs, oldVal, newVal) -> filterLobbies());
+        ComboBox3.valueProperty().addListener((obs, oldVal, newVal) -> filterLobbies());
+
+        // Inizializza la lista filtrata con tutte le lobby
+        filteredLobbies.setAll(allLobbies);
+        updateLobbyView();
+
+        // Imposta la cell factory per la ListView
+        lobbyListView.setCellFactory(lv -> new ListCell<Lobby>() {
+            @Override
+            protected void updateItem(Lobby item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getName() + " - " + item.getType() + " - " + item.getDuration() + " - " + item.getNumPlayers() + " players");
+                }
+            }
+        });
+    }
 
     @FXML
     void onClickUser(ActionEvent event) throws IOException {
         goToLogin();
     }
 
-@FXML
+    @FXML
     private void goToLogin() throws IOException {
-        // Carica il file FXML della seconda scena
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
         Parent root = loader.load();
-
-        // Ottieni lo stage attuale
-        Stage stage = (Stage) HomePane.getScene().getWindow(); // Alternativa: (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-        // Crea una nuova scena e impostala nello stage
+        Stage stage = (Stage) HomePane.getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
     }
 
+    private void filterLobbies() {
+        // Ottieni i valori selezionati
+        String type = ComboBox1.getValue();
+        String duration = ComboBox2.getValue();
+        String numPlayersStr = ComboBox3.getValue();
+
+        // Filtra le lobby
+        filteredLobbies.setAll(allLobbies.filtered(lobby -> {
+            boolean matchesType = (type == null || lobby.getType().equals(type));
+            boolean matchesDuration = (duration == null || lobby.getDuration().equals(duration));
+            boolean matchesPlayers = (numPlayersStr == null || lobby.getNumPlayers() == Integer.parseInt(numPlayersStr));
+            return matchesType && matchesDuration && matchesPlayers;
+        }));
+
+        // Aggiorna la vista
+        updateLobbyView();
+    }
+
+    private void updateLobbyView() {
+        // Aggiorna la ListView con le lobby filtrate
+        lobbyListView.setItems(filteredLobbies);
+    }
+
+    // Classe per rappresentare una lobby
+    public static class Lobby {
+        private String name;
+        private String type; // Online/Presenza
+        private String duration; // Singola/Campagna
+        private int numPlayers;
+
+        public Lobby(String name, String type, String duration, int numPlayers) {
+            this.name = name;
+            this.type = type;
+            this.duration = duration;
+            this.numPlayers = numPlayers;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getDuration() {
+            return duration;
+        }
+
+        public int getNumPlayers() {
+            return numPlayers;
+        }
+
+        @Override
+        public String toString() { //solo per visualizzare
+            return name + " - " + type + " - " + duration + " - " + numPlayers + " players";
+        }
+    }
 }
---
+++ Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/Player.java
@@ -0,0 +1,43 @@
+package it.uniroma2.marchidori.entity;
+
+import java.util.List;
+
+public class Player {
+
+    private final String id;
+    private final String username;
+    //private final List<CharacterSheet> characterSheets;  // Riferimento ai fogli dei personaggi
+    private final List<it.uniroma2.marchidori.entity.Lobby> favouriteLobbies;  // Riferimento alle lobby preferite
+   // private final List<Notification> notificationPlayer;  // Riferimento alle notifiche
+
+    // Costruttore immutabile
+    public Player(String id, String username, //List<CharacterSheet> characterSheets,
+                  List<it.uniroma2.marchidori.entity.Lobby> favouriteLobbies)/*, List<Notification> notificationPlayer)*/ {
+        this.id = id;
+        this.username = username;
+       // this.characterSheets = characterSheets;
+        this.favouriteLobbies = favouriteLobbies;
+        //this.notificationPlayer = notificationPlayer;
+    }
+
+    // Getter
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    /*public List<CharacterSheet> getCharacterSheets() {
+        return characterSheets;
+    }*/
+
+    public List<it.uniroma2.marchidori.entity.Lobby> getFavouriteLobbies() {
+        return favouriteLobbies;
+    }
+
+    /*public List<Notification> getNotificationPlayer() {
+        return notificationPlayer;
+    }*/
+}
