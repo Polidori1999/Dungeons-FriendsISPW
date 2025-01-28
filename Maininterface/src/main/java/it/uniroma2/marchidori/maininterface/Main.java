9e9f760cb54f82b33816d9660a1d429f606ec59e
kuro1999 <edoardo.marchionni99@gmail.com>
2025-01-28 17:52:51 +0100

    invio solo charcatersjeet

--- Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/Main.java
+++ Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setResizable(true);
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Home.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setTitle("Login_Screen");
         primaryStage.setScene(scene);
--- Maininterface/src/main/java/module-info.java
+++ Maininterface/src/main/java/module-info.java
@@ -5,9 +5,14 @@ module it.uniroma2.marchidori.maininterface {
     requires javafx.graphics;
     requires java.desktop;
 
-
+    // Apre il package principale ai file FXML (controller, ecc.)
     opens it.uniroma2.marchidori.maininterface to javafx.fxml;
     exports it.uniroma2.marchidori.maininterface;
+
+    // Apre / esporta l'entity
     exports it.uniroma2.marchidori.maininterface.entity;
     opens it.uniroma2.marchidori.maininterface.entity to javafx.fxml;
+
+    // Aggiungi questa riga per permettere a javafx.base di riflettere sulle classi nel package .bean
+    opens it.uniroma2.marchidori.maininterface.bean to javafx.base;
 }
