7a83c8416e454e2425896c03b9bdbfb81070cbb9
polidori1999 <leo.polidori99@gmail.com>
2025-01-30 10:50:16 +0100

    connessione con sonarlint plugin sonar cloud

--- Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/entity/Player.java
+++ Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/entity/Player.java
@@ -8,15 +8,16 @@ public class Player {
     private final String username;
     //private final List<CharacterSheet> characterSheets;  // Riferimento ai fogli dei personaggi
     private final List<it.uniroma2.marchidori.maininterface.entity.Lobby> favouriteLobbies;  // Riferimento alle lobby preferite
-   // private final List<Notification> notificationPlayer;  // Riferimento alle notifiche
+    // private final List<Notification> notificationPlayer;  // Riferimento alle notifiche
 
     // Costruttore immutabile
     public Player(String id, String username, //List<CharacterSheet> characterSheets,
                   List<it.uniroma2.marchidori.maininterface.entity.Lobby> favouriteLobbies)/*, List<Notification> notificationPlayer)*/ {
         this.id = id;
         this.username = username;
-       // this.characterSheets = characterSheets;
         this.favouriteLobbies = favouriteLobbies;
+        //DA IMPLEMENTARE
+        //this.characterSheets = characterSheets;
         //this.notificationPlayer = notificationPlayer;
     }
 
@@ -29,6 +30,7 @@ public class Player {
         return username;
     }
 
+    //DA IMPLEMENTARE
     /*public List<CharacterSheet> getCharacterSheets() {
         return characterSheets;
     }*/
@@ -37,6 +39,7 @@ public class Player {
         return favouriteLobbies;
     }
 
+    //DA IMPLEMENTARE
     /*public List<Notification> getNotificationPlayer() {
         return notificationPlayer;
     }*/
