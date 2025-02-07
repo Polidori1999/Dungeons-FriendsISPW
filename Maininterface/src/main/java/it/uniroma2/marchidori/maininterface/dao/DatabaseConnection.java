08d023b208011fc3bdc259922122699eccd26e01
polidori1999 <leo.polidori99@gmail.com>
2025-02-07 19:17:08 +0100

    risolto il mega codesmell rosso per la password del db visibile

--- Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/dao/DatabaseConnection.java
+++ Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/dao/DatabaseConnection.java
@@ -1,13 +1,28 @@
 package it.uniroma2.marchidori.maininterface.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/dnf";
-    private static final String USER = "root";
-    private static final String PASSWORD = "admin";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try (FileInputStream fis = new FileInputStream("src/main/resources/db_config.properties")) {
+            Properties properties = new Properties();
+            properties.load(fis);
+            URL = properties.getProperty("db.url");
+            USER = properties.getProperty("db.user");
+            PASSWORD = properties.getProperty("db.password");
+        } catch (IOException e) {
+            throw new RuntimeException("❌ Errore nel caricamento della configurazione del database", e);
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
