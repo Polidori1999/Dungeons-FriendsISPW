384bb56ef7747e5ca6344f100b8d18b1244e1f0d
polidori1999 <leo.polidori99@gmail.com>
2025-02-11 18:18:16 +0100

    nonè vero non funzionava

--- Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/Main.java
+++ Maininterface/src/main/java/it/uniroma2/marchidori/maininterface/Main.java
@@ -14,6 +14,7 @@ import java.util.logging.Logger;
 
 public class Main extends Application {
 
+
     // Creazione del logger
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
